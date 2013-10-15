49f2d35bd3db68c45dc5b0fea768651145233484
John Richardson <richardsonjm@gmail.com>
2013-10-15 13:56:19 -0400

    song specs

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,19 +1,27 @@
-require "spec_helper.rb"
+require_relative './spec_helper.rb'
 
 describe "Song" do
   it "can initialize a song" do
-    pending #implement this spec
+    Song.new.should be_an_instance_of(Song)
   end
 
   it "can have a name" do
-    pending #implement this spec
+    song = Song.new
+    song.name = "Sabatoge"
+    song.name.should eq("Sabatoge")
   end
 
   it "can have a genre" do
-    pending #implement this spec
+    song = Song.new
+    song.genre = Genre.new.tap { |genre| genre.name = "rap" }
+    song.genre.name.should eq("rap")
   end
 
   it "has an artist" do
-    pending #implement this spec
+    artist = Artist.new
+    song = Song.new
+    artist.add_song(song)
+    song.artist.should == artist
+    song.artist.class.should == Artist
   end
 end
